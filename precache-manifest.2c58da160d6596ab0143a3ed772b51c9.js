4c143c0abcaa175a71007dae48a85579d3abfe80
Steven Ortiz <stevenortiz.es@gmail.com>
2020-02-13 20:50:05 +0000

    Updates

---
+++ precache-manifest.2c58da160d6596ab0143a3ed772b51c9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f064af55b852bc96b9bc9b8d572f6315",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc44c99fc0ddf66a08d4",
+    "url": "/static/css/main.ff852559.chunk.css"
+  },
+  {
+    "revision": "9c6efcbfaf4d68de9ab1",
+    "url": "/static/js/2.6c1df444.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.6c1df444.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc44c99fc0ddf66a08d4",
+    "url": "/static/js/main.231096f8.chunk.js"
+  },
+  {
+    "revision": "ccd1adbe065bbf296b21",
+    "url": "/static/js/runtime-main.7c7f9f47.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/static/media/crown.ed28accb.svg"
+  }
+]);
