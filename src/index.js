e12f827994c7e493df363c8e826f055d639ce7b6
Steven Ortiz <stevenortiz.es@gmail.com>
2020-02-18 19:42:21 +0000

    Configure redux and add user reducer

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
+import {Provider} from 'react-redux';
+
+import myStore from './redux/store'
+
 import './index.css';
 import App from './App';
 
 ReactDOM.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>,
+    <Provider store={myStore}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>,
     document.getElementById('root')
 );
 
