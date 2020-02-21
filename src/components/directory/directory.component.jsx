f87931aebb4a0d29e0cdf2682f265eb425e5a4ef
Steven Ortiz <stevenortiz.es@gmail.com>
2020-02-21 19:11:31 +0000

    Add directory data to redux

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,46 +1,13 @@
 import React from 'react';
+import {createStructuredSelector} from "reselect";
 
 import MenuItem from "components/menu-item/menu-item.component";
+import {connect} from "react-redux";
+import {selectDirectorySections} from "redux/directory/directory.selector";
 
 import './directory.styles.scss'
 
-const Directory = () => {
-
-    const sections = [
-        {
-            title: 'hats',
-            imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-            id: 1,
-            linkUrl: 'shop/hats'
-        },
-        {
-            title: 'jackets',
-            imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-            id: 2,
-            linkUrl: 'shop/jackets'
-        },
-        {
-            title: 'sneakers',
-            imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-            id: 3,
-            linkUrl: 'shop/sneakers'
-        },
-        {
-            title: 'womens',
-            imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-            size: 'large',
-            id: 4,
-            linkUrl: 'shop/womens'
-        },
-        {
-            title: 'mens',
-            imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-            size: 'large',
-            id: 5,
-            linkUrl: 'shop/mens'
-        }
-    ];
-
+const Directory = ( { sections }) => {
     return (
         <div className='directory-menu'>
             {sections.map(({id, ...otherSectionProps}) => (
@@ -50,4 +17,8 @@ const Directory = () => {
     )
 };
 
-export default Directory;
+const mapStateToProps = createStructuredSelector( {
+    sections: selectDirectorySections
+});
+
+export default connect(mapStateToProps)(Directory);
