e06e29d0474614cb9094401bf426e4c5db40968d
Steven Ortiz <stevenortiz.es@gmail.com>
2020-02-21 18:46:04 +0000

    Add local storage persistence

--- src/index.js
+++ src/index.js
@@ -2,16 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
 import {Provider} from 'react-redux';
+import { PersistGate } from "redux-persist/integration/react";
 
-import myStore from './redux/store'
+import { store, persistor } from './redux/store'
 
 import './index.css';
 import App from './App';
 
 ReactDOM.render(
-    <Provider store={myStore}>
+    <Provider store={store}>
         <BrowserRouter>
-            <App />
+            <PersistGate persistor={persistor}>
+                <App />
+            </PersistGate>
         </BrowserRouter>
     </Provider>,
     document.getElementById('root')
